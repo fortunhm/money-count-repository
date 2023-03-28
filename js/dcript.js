eb520ae257c88a970c1de57905a006ff1396c6f8
Md.Hasan <hasanpc30@gmail.com>
2023-03-28 13:32:19 +0600

    some comment out push

--- js/dcript.js
+++ js/dcript.js
@@ -1,3 +1,9 @@
 document.getElementById('calculate-btn').addEventListener('click', function(){
+    const incomeCost = document.getElementById('income').value;
+    const income = parseInt(incomeCost);
+
+    // expense item adding
+    const foodCost = document.getElementById('food').value;
+    const food = parseInt(foodCost);
     
 })
