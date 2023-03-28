dd3feb296146e91a53b269776ebc8dec34389dc6
Md.Hasan <hasanpc30@gmail.com>
2023-03-28 16:12:32 +0600

    finel add

--- js/dcript.js
+++ js/dcript.js
@@ -1,9 +1,69 @@
-document.getElementById('calculate-btn').addEventListener('click', function(){
-    const incomeCost = document.getElementById('income').value;
-    const income = parseInt(incomeCost);
+const valueDisplay = (id, value) => {
+    const totalBalance = document.getElementById(id)
+    totalBalance.innerText = value;
 
+}
+// add some function ---
+function getElement(id) {
+    const getElm = document.getElementById(id);
+    const element = parseInt(getElm.value)
+    return element
+}
+// calculate button site creating ----
+document.getElementById('calculate-btn').addEventListener('click', function () {
+
+    const income = getElement('income')
     // expense item adding
-    const foodCost = document.getElementById('food').value;
-    const food = parseInt(foodCost);
-    
+    // food is add ----
+    // const foodCost = document.getElementById('food').value;
+    const food = getElement('food');
+    // rent is add ---
+    const rentCost = document.getElementById('rent').value;
+    const rent = parseInt(rentCost);
+    // clothes is add ---
+    const clothesCost = document.getElementById('clothes').value;
+    const clothes = parseInt(clothesCost);
+    // condition applied ---
+    if (
+        income < 0 ||
+        income == "" ||
+        food < 0 ||
+        food == "" ||
+        rent < 0 ||
+        rent == "" ||
+        clothes < 0 ||
+        clothes == ""
+    ) {
+        alert('please give this input field positive number');
+        return;
+    }
+    // all ar connect ---
+    const expenses = food + rent + clothes;
+    const balance = income - expenses;
+    // total balance display add ---
+    const totalExpenses = document.getElementById('total-expense');
+    totalExpenses.innerText = expenses;
+    // const totalBalance = document.getElementById('balance');
+    // totalBalance.innerText = balance;
+    valueDisplay('balance', balance)
+})
+
+// saving button site making -----
+document.getElementById('save-btn').addEventListener('click', function () {
+    // income input ---
+    const income = document.getElementById('income').value;
+    // save input ---
+    const save = document.getElementById('save').value;
+    // save percentage site ---
+    const saveAmount = (save / 100) * income;
+    // get balance amount ---
+    const balance = document.getElementById('balance').innerText;
+    const addedNew = balance - saveAmount;
+    // saving amount and remaining balance add ---
+    if (saveAmount > balance) {
+        alert("please reset fast, saving amount high to balance");
+    } else {
+        document.getElementById("saving-amount").innerText = saveAmount;
+        document.getElementById("remaining-balance").innerText = addedNew;
+    }
 })
